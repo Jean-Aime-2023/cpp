e797cfa9644f5e24a48f856359d18a12e3824efd
Jean-Aime-2023 <124204227+Jean-Aime-2023@users.noreply.github.com>
2024-03-16 20:56:06 +0200

    review

--- 2InsertionSort.cpp
+++ 2InsertionSort.cpp
@@ -1,41 +1,15 @@
-#include <bits/stdc++.h>
+#include<iostream>
 using namespace std;
 
-void insertionSort(int arr[],int n){
-	int i,j,key;
-	
-	for(i=1;i<n;i++){
-		key = arr[i];
-		j=i-1;
-		
-		while(j>=0 && arr[j]>key){
-		arr[j+1] = arr[j];
-		j=j-1;
-	}
-	
-	arr[j+1] = key;
-		
-	}
-	
-}
-
-void printArr(int arr[],int n){
-	int i;
-	for(i=0;i<n;i++){
-		cout<<arr[i]<<endl;
+void selectionSort(int arr[],int n){
+	int i,j;
+	for(i=0;i<n-1;i++){
+		int targetIndex = i;
+		for(j=i+1;j<n;j++){
+			if(arr[j]<arr[targetIndex]){
+				targetIndex = j;
+			}
+		}
+		swap(arr[j],arr[targetIndex]);
 	}
 }
-
-int main(){
-
-int arr[] = {50,14,62,90,8};
-int n = sizeof(arr) / sizeof(arr[0]);
-
-insertionSort(arr,n);
-printArr(arr,n);
-
-return 0;
-	
-}
-
-
