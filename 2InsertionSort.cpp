25b9af854161f605cc6ef800dfca7809a55c23c6
Jean-Aime-2023 <124204227+Jean-Aime-2023@users.noreply.github.com>
2024-03-16 21:49:20 +0200

    updates

--- 2InsertionSort.cpp
+++ 2InsertionSort.cpp
@@ -1,15 +1,79 @@
 #include<iostream>
 using namespace std;
 
+void insertionSort(int arr,int n){
+	int i,j,key;
+	for(i=1;i<n;i++){
+		key = arr[i];
+		j = i-1;
+		while(j>=0 && arr[j]>key){
+			arr[j+1] = arr[j];
+			j = j-1;
+		}	
+		arr[j+1] = key;
+	}
+}
+
 void selectionSort(int arr[],int n){
-	int i,j;
+	int i,j,targetIndex;
 	for(i=0;i<n-1;i++){
-		int targetIndex = i;
+		targetIndex = i;
 		for(j=i+1;j<n;j++){
-			if(arr[j]<arr[targetIndex]){
+			if(arr[j]>arr[targetIndex]){
 				targetIndex = j;
 			}
 		}
 		swap(arr[j],arr[targetIndex]);
+	}
+}
+
+void mergeSort(int a[] , int s , int e){
+	if(s>=e){
+		return;
+	}
+	
+	int mid = (s+e)/2;
+	int x[100],int y[100];
+	
+	for(int i=s; i<mid;i++){
+		x[i] = a[i];
+	}	
+	for(int i=mid+1; i<e;i++){
+		y[i] = a[i];
+	}
+	
+	mergeSort(x,s,mid);
+	mergeSort(y,mid+1,e);
+	mergeArrays(x,y,a,s,e);
+}
+
+void mergeArrays(int x[] ,int y[] , int a[] , s, e){
+	int mid = (s+e)/2;
+	int i = s;
+	int k = s;
+	int j = mid + 1;
+	
+	while(i<=mid && j<=e){
+		if(x[i]>y[i]){
+			a[k] = x[i];
+			i++;
+			k++;
+		}else{
+			a[k] = y[i];
+			i++;
+			k++;
+		}
+	}
+	
+	while(i<=mid){
+		a[k] = x[i];
+			i++;
+			k++;
+	}
+	
+	while(j<=e){
+		a[k] = y[i];
+			i++;
+			k++;
 	}
 }
